18b69d0650eba6c0e472b2fb3d5369a97bf518ca
Olena Ageyeva <oageyeva@Commonwealth.com>
2019-10-13 16:27:12 -0400

    unified json data read added

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,18 @@ import React, { useState } from "react";
 import ContainerStyles from "./Container.styles";
 
 export default function Container() {
+  const apiPool = [
+    "https://dog.ceo/api/breeds/image/random",
+    "https://random.dog/woof.json",
+    "https://api.thecatapi.com/v1/images/search"
+    //"https://source.unsplash.com/random"
+  ];
+
+  function checkURL(url) {
+    console.log(url);
+    return url.match ? url.match(/\.(jpeg|jpg|gif|png)$/) != null : false;
+  }
+
   function getRandomColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
@@ -12,10 +24,31 @@ export default function Container() {
     return color;
   }
 
+  function findImage(object) {
+    var image;
+    for (var key in object) {
+      if (checkURL(object[key])) {
+        image = object[key];
+      } else {
+        if (typeof object[key] === "object") {
+          for (var item in object[key]) {
+            if (checkURL(object[key][item])) {
+              image = object[key][item];
+            }
+          }
+        }
+      }
+    }
+    return image;
+  }
+
   function getRandomDog() {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    const api = Math.floor(Math.random() * 3);
+    fetch(apiPool[api])
       .then(response => response.json())
-      .then(json => setBgimage(json.message));
+      .then(json => setBgimage(findImage(json)));
+
+    console.log(api);
   }
 
   const [bgcolor, setBgcolor] = useState(getRandomColor);
